f395133f62d9fdfb7f245c5682f15499158926cb
kyle Parry <parryk13@gmail.com>
2016-10-20 18:10:15 -0400

    hey I did a thing

--- helloworld.c
+++ helloworld.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
   printf("Hello World!\n");
 
   printf("Git is cool!\n", ); //wow such awesome much cool
+      //hey look i made a change
 
   printf("I'm using git!\n", );
   return 0;
